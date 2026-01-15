edd686347646ab6457920b2b40abcce9a7626c80
saurav <echo.saurav@gmail.com>
2026-01-16 03:13:18 +0600

    added tiled window manager

---
+++ .config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  sketchybar --set $NAME background.drawing=on
+else
+  sketchybar --set $NAME background.drawing=off
+fi
+
+# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+#   sketchybar --set $NAME background.color=#ff6a127f
+# else
+#   sketchybar --set $NAME background.color=#ff7295bd
+# fi
