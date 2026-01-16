9160a01a816f9eeef3c228ad11d01170a4b3efda
saurav <echo.saurav@gmail.com>
2026-01-17 03:27:45 +0600

    on click open activity

--- .config/sketchybar/plugins/ram.sh
+++ .config/sketchybar/plugins/ram.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #ram.sh
 
-sketchybar -m --set "$NAME" label="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')%"
+sketchybar -m --set "$NAME" \
+label="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')%" \
+click_script="open /System/Applications/Utilities/Activity\ Monitor.app"
